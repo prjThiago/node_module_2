5af6df058af0dee826e62228c262c3f3317a1f83
Thiago morais <thiago.morais@sellbie.com.br>
2019-06-07 09:17:45 -0300

    commit de appointments

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,17 @@ const guestMiddleware = require('./app/middlewares/guest')
 
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
+const DashboardController = require('./app/controllers/DashboardController')
+const FileController = require('./app/controllers/FileController')
+
+routes.use((req, res, next) => {
+  res.locals.flashSuccess = req.flash('success')
+  res.locals.flashError = req.flash('error')
+
+  return next()
+})
+
+routes.get('/files/:file', FileController.show)
 
 routes.get('/', guestMiddleware, SessionController.create)
 routes.post('/signin', SessionController.store)
@@ -17,10 +28,7 @@ routes.get('/signup', guestMiddleware, UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
 
 routes.use('/app', authmiddleware)
-routes.get('/app/logout', SessionController.destroy())
+routes.get('/app/logout', SessionController.destroy)
 
-routes.get('/app/dashboard', (req, res) => {
-  console.log(req.session.user)
-  res.render('dashboard')
-})
+routes.get('/app/dashboard', DashboardController.index)
 module.exports = routes
